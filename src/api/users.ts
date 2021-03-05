d20d1ae711a4668ee8306ffbdcfbfaccdf91fbde
psi <cy.lonsido@gmail.com>
2021-03-06 02:38:07 +0800

    update request url path

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,53 +2,53 @@ import request from '@/utils/request'
 
 export const getUsers = (params: any) =>
   request({
-    url: '/users',
+    url: '/user',
     method: 'get',
     params
   })
 
 export const getUserInfo = (data: any) =>
   request({
-    url: '/users/info',
-    method: 'post',
+    url: '/user/info',
+    method: 'get',
     data
   })
 
 export const getUserByName = (username: string) =>
   request({
-    url: `/users/${username}`,
+    url: `/user/${username}`,
     method: 'get'
   })
 
 export const updateUser = (username: string, data: any) =>
   request({
-    url: `/users/${username}`,
+    url: `/user/${username}`,
     method: 'put',
     data
   })
 
 export const deleteUser = (username: string) =>
   request({
-    url: `/users/${username}`,
+    url: `/user/${username}`,
     method: 'delete'
   })
 
 export const login = (data: any) =>
   request({
-    url: '/users/login',
+    url: '/login',
     method: 'post',
     data
   })
 
 export const logout = () =>
   request({
-    url: '/users/logout',
+    url: '/user/logout',
     method: 'post'
   })
 
 export const register = (data: any) =>
   request({
-    url: '/users/register',
+    url: '/user/register',
     method: 'post',
     data
   })
